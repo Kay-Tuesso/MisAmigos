74db68806eea17afac130cb7b0b1c240aad1929a
Jason Heintz <jitops@hotmail.com>
2019-06-13 09:48:00 -0700

    Plus Bing

---
+++ MisAmigos/MisAmigos/BingMapsHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MisAmigos.Helpers
+{
+    public class BingMapsHelper
+    {
+        delegate void Operation(Uri url);
+        public bool Run(Uri url)
+        {
+            bool success = false;
+            Operation op = GetAsync;
+            ExecOperation(url, op);
+            return success;
+        }
+               
+        public static void GetAsync(Uri url)
+        {
+            Console.WriteLine($"Url: {url.AbsoluteUri}");
+            Console.WriteLine($"Url: {url.Fragment}");
+        }
+
+        static void ExecOperation(Uri url, Operation op)
+        {
+            op(url);
+        }
+    }
+}
